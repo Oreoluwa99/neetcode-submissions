10720b6b35f90ccd87da0fdc561ba437cbf12019
Oreoluwa Alade <76736750+Oreoluwa99@users.noreply.github.com>
2026-04-20 05:37:14 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,44 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s)) 
+            anagrams[key].append(s)
+        return list(anagrams.values())
+
+# The time complexity here is O(n * mlogm)
+# The space complexity here is O(m*n) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagrams = defaultdict(list) # create a default value for a key if it doesn't exist
+
+#         for s in strs:
+#             key = ''.join(sorted(s))
+#             anagrams[key].append(s)
+#         return list(anagrams.values())
+
+# # The time complexity here is O(n * mlogm)
+# # The space complexity here is O(m*n) 
