92b5554bf50c760516d48e9a9d3520de8b5de480
Oreoluwa Alade <76736750+Oreoluwa99@users.noreply.github.com>
2026-05-14 13:05:38 -0500

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,58 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                maxProfit = prices[r] - prices[l] # calculate the profit
+                maxP = max(maxProfit, maxP)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+# time complexity is O(n)
+# space complexity is O(1)
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         l = 0
+#         r = 1
+#         maxP = 0
+
+#         while r < len(prices):
+#             if prices[l] < prices[r]:
+#                 profit = prices[r] - prices[l]
+#                 maxP = max(maxP, profit)
+#             else:
+#                 l = r
+#             r += 1
+#         return maxP
